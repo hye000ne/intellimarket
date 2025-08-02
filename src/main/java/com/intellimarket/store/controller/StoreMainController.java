e76a1470045d130ab8e7885ce2e9569c149eb5c8
rekindle402 <rekindle402@gmail.com>
2025-08-03 04:07:16 +0900

    Feat: 스토어 판매 페이지 기능 구현 중

--- src/main/java/com/intellimarket/store/controller/StoreMainController.java
+++ src/main/java/com/intellimarket/store/controller/StoreMainController.java
@@ -1,13 +1,59 @@
 package com.intellimarket.store.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.intellimarket.store.domain.Product;
+import com.intellimarket.store.domain.Seller;
+import com.intellimarket.store.domain.StoreCategory;
+import com.intellimarket.store.domain.StoreInfo;
+import com.intellimarket.store.service.ProductService;
+import com.intellimarket.store.service.StoreCategoryService;
+import com.intellimarket.store.service.StoreInfoService;
 
 @Controller	
 @RequestMapping("/store")
 public class StoreMainController {
+	
+	@Autowired
+	StoreInfoService storeInfoService;
+	
+	@Autowired
+	StoreCategoryService storeCategoryService;
+
+	@Autowired
+	ProductService productService;
+	
+	/**
+	 * 쇼핑몰 메인 페이지
+	 */
+	@GetMapping("/")
+	public String main(Model model, @RequestParam("sellerId") int sellerId, HttpSession session) {
+	    // sellerId를 파라미터로 받아서 서비스 메서드 호출
+		Seller seller = new Seller();
+		seller.setSellerId(sellerId);
+		
+	    StoreInfo storeInfo = storeInfoService.selectById(seller);
+
+	    List<StoreCategory> storeCategories = storeCategoryService.getAllCategoryById(seller);
+	    List<Product> products = productService.selectById(sellerId);
+
+	    model.addAttribute("storeInfo", storeInfo);
+	    model.addAttribute("storeCategories", storeCategories);
+	    model.addAttribute("products", products);
+	    model.addAttribute("contentPage", "store/intelli/main.jsp");
+
+	    return "layout/intelliStore";
+	}
+	
 	
 	/**
 	 * 마켓 판매자용 소개 페이지
